4bf22458529fa475f92562d32bddc48e20cca46b
fungleo <web@fengcms.com>
2021-05-14 18:07:06 +0800

    完成选择语言组件

---
+++ src/components/common/SelectLang.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { languages } from '@/lang'
+
+const SelectLang: FC = () => {
+  const { i18n } = useTranslation()
+  const changelang = (code: string): void => {
+    i18n.changeLanguage(code).catch(() => {})
+  }
+  return (
+    <div className="select-lang">
+      <h3>切换语言</h3>
+      <ul>
+        {
+          languages.map(({ label, value }) => {
+            return (
+              <li onClick={() => changelang(value)} key={value}>{label}</li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default SelectLang
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,11 @@ import App from '@/views/App'
 import './style/style.scss'
 import reportWebVitals from './reportWebVitals'
 import '@/lang'
+import SelectLang from '@/components/common/SelectLang'
 
 ReactDOM.render(
   <StrictMode>
+    <SelectLang />
     <App />
   </StrictMode>,
   document.getElementById('root')
