5f95a6adc7d5ebf87d5cf24d95919c16dda40e6b
fungleo <web@fengcms.com>
2021-08-28 20:13:35 +0800

    fix bug

--- config-overrides.js
+++ config-overrides.js
@@ -19,12 +19,6 @@ function invade (target, name, callback) {
     }
   )
 }
-const getCssLoaders = config => {
-  const loaderFilter = rule => rule.loader && rule.loader.includes('/css-loader')
-  let loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf
-  loaders = loaders.reduce((ldrs, rule) => ldrs.concat(rule.use || []), [])
-  return loaders.filter(loaderFilter)
-}
 
 module.exports = {
   paths: function (paths) {
@@ -44,10 +38,6 @@ module.exports = {
           // 去除生产环境 console.log
           e.options.terserOptions.compress.drop_console = true
         })
-      }
-      // Fix url('/images/....') being processed by css-loader 4 =/
-      for (const loader of getCssLoaders(config)) {
-        loader.options.url = url => url.startsWith('/')
       }
       return config
     }
